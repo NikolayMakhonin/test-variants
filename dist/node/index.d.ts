b34a087437e50f56aed5ed1189719b88b545c672
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2022-05-23 14:54:30 +0200

    develop: Tests OK

--- dist/node/index.d.ts
+++ dist/node/index.d.ts
@@ -1,3 +1,1 @@
-export { IAbortSignal, IAbortController, AbortSignalClass, AbortControllerClass, } from './abort-controller/contracts';
-export * from './abort-controller/original/index';
-export * from './abort-controller/fast/index';
+export { createTestVariantsSync, createTestVariants, } from './test-variants/createTestVariants';
