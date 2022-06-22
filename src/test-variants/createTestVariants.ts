4485b40adfa5190efa138a165b364052023a2455
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2022-06-22 12:08:31 +0400

    v0.0.12

--- src/test-variants/createTestVariants.ts
+++ src/test-variants/createTestVariants.ts
@@ -99,7 +99,7 @@ export function createTestVariants<TArgs extends object>(
 
       function onError(err) {
         console.error(JSON.stringify(variantArgs, null, 2))
-        console.error(err) // required to show intelliJ idea assertion difference
+        // console.error(err) // required to show intelliJ idea assertion difference
 
         // rerun failed variant 5 times for debug
         const time0 = Date.now()
