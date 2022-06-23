1b86f0d6ce8c32d813cd8c60f16be5057eb10dfa
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2022-06-23 05:29:05 +0400

    v0.0.14

--- src/test-variants/createTestVariants.ts
+++ src/test-variants/createTestVariants.ts
@@ -44,7 +44,7 @@ export function createTestVariants<TArgs extends object>(
 ): TestVariantsSetArgs<TArgs> {
   return function testVariantsArgs(args) {
     return function testVariantsCall({
-      pauseIterationsAsync = 10000,
+      pauseIterationsAsync = 5000,
       pauseInterval = 1000,
       pauseTime = 10,
       logInterval = 10000,
