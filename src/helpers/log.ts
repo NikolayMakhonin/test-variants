73483fafd9812eb9842dce798d2711d3a30a5c2c
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2025-12-31 12:32:53 +0700

    dev: refactor
    Took 34 minutes

---
+++ src/helpers/log.ts
@@ -0,0 +1,52 @@
+import {objectToString} from 'src/test-variants/format/objectToString'
+
+let traceIndent = 0
+
+const LOG_LAST_MAX = 1000
+const logLast: string[] = []
+
+export function formatValue(value: unknown): string {
+  return objectToString(value, {maxDepth: 3, maxItems: 20})
+}
+
+export function formatObject(obj: unknown): string {
+  return objectToString(obj, {pretty: true, maxDepth: 5, maxItems: 50})
+}
+
+export function log(...args: unknown[]): void {
+  const message = args.map(a => typeof a === 'string' ? a : formatObject(a)).join(' ')
+  logLast.push(message)
+  if (logLast.length > LOG_LAST_MAX) {
+    logLast.shift()
+  }
+  console.log(message)
+}
+
+export function traceLog(...args: unknown[]): void {
+  const message = args.map(a => typeof a === 'string' ? a : formatValue(a)).join(' ')
+  log('  '.repeat(traceIndent) + message)
+}
+
+export function traceEnter(...args: unknown[]): void {
+  const message = args.map(a => typeof a === 'string' ? a : formatValue(a)).join(' ')
+  traceLog('> ' + message)
+  traceIndent++
+}
+
+export function traceExit(...args: unknown[]): void {
+  traceIndent--
+  const message = args.map(a => typeof a === 'string' ? a : formatValue(a)).join(' ')
+  traceLog('< ' + message)
+}
+
+export function getLogLast(): string {
+  return logLast.join('\n')
+}
+
+export function resetLog(): void {
+  traceIndent = 0
+  logLast.length = 0
+}
+
+// eslint-disable-next-line no-undef
+(globalThis as any).__getStressTestLogLast = getLogLast
