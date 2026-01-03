8153c9060779e9b36d5bddf8f4ffab950d44d516
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2026-01-03 10:30:33 +0700

    dev: refactor

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,5 +1,4 @@
 export {
-  type AddLimitOptions,
   type GenerateErrorVariantFilePathOptions,
   type GetSeedParams,
   type LimitArgOnError,
@@ -8,30 +7,14 @@ export {
   type SaveErrorVariantsOptions,
   type TestVariantsBestError,
   type TestVariantsFindBestErrorOptions,
-  type TestVariantsIterator,
-  type TestVariantsIteratorLimit,
-  type TestVariantsIteratorOptions,
   type TestVariantsLogOptions,
   type TestVariantsRunOptions,
   type TestVariantsRunResult,
-  type TestVariantsTemplate,
-  type TestVariantsTemplates,
 } from 'src/common/test-variants/types'
-export { generateErrorVariantFilePath } from 'src/common/test-variants/saveErrorVariants'
 export {
   type ErrorEvent,
   type OnErrorCallback,
-  type TestVariantsTest,
   type TestVariantsTestOptions,
   type TestVariantsTestResult,
-  type TestVariantsCreateTestRunOptions,
-  type TestVariantsTestRun,
-  type TestVariantsTestRunResult,
 } from 'src/common/test-variants/testVariantsCreateTestRun'
-export {
-  type TestVariantsTemplatesExt,
-  type TestVariantsSetArgs,
-  type TestVariantsCall,
-  createTestVariants,
-} from './test-variants/createTestVariants'
-export { testVariantsIterator } from 'src/common/test-variants/testVariantsIterator'
+export { createTestVariants } from './test-variants/createTestVariants'
